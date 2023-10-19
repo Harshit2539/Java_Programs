2910bd74e5649b142692dccf05480ffd4c1dd242
HARSHIT TYAGI <133091460+Harshit2539@users.noreply.github.com>
2023-10-19 11:39:05 +0530

    Update Armstrong_Function.java

--- Armstrong_Function.java
+++ Armstrong_Function.java
@@ -4,30 +4,49 @@ import java.util.*;
 
 public class Armstrong_Function {
 
-    static int calculation(int a,int s){
-        int r;
-        while(a > 0){
-            r = a % 10;
-            s = s + r*r*r;
-            a = a / 10;
+
+    static int Lenght(int a) {
+
+        int c = 0;
+        while (a > 0) {
+            a = a/10;
+            c++;
         }
-        return (s);
+        return c;
+    }
+    static int Power(int b, int e) {
+        int pv = 1;
+        for (int i = 1; i <= e; i++) {
+            pv = pv*b;
+        }
+        return pv;
+    }
+
+    static int Armstrongnumber(int n) {
+        int m = n;
+        int nl = Lenght(m);
+        int sum = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            sum += Power(lastDigit, nl);
+            n /= 10;
+        }
+
+        return sum;
 
     }
+
     public static void main(String[] args) {
         System.out.println("Enter the number");
          Scanner sc = new Scanner (System.in);
          int n = sc.nextInt();
-         int m=n;
-         int sum=0;
-         sum = calculation(n,sum);
-
-         if(sum==m)
+        int v = Armstrongnumber(n);
+        if(n==v)
             {
-                System.out.println("The Armstrong number of is: "+sum);
+                System.out.println("The Armstrong number of is: "+n);
             }
             else{
-                System.out.println("Not an Armstrong Number "+m);
+                System.out.println("Not an Armstrong Number "+n);
             }
             sc.close();
     }
