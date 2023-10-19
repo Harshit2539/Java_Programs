683cdcf46c57e0b67374a26316bdf657e04f4c77
HARSHIT TYAGI <133091460+Harshit2539@users.noreply.github.com>
2023-10-19 22:17:56 +0530

    Update Leap_Year.java

--- Leap_Year.java
+++ Leap_Year.java
@@ -9,12 +9,12 @@ public class Leap_Year {
             int year = sc.nextInt();
             if (year % 4 == 0) {
                 if (year % 400 == 0) {
-                        System.out.println(year + " is a leap year.");
-                    } else {
                         System.out.println(year + " is not a leap year.");
+                    } else {
+                        System.out.println(year + " is  a leap year.");
                     }
                 } else {
-                    System.out.println(year + " is a leap year.");
+                    System.out.println(year + " is not a leap year.");
                 }
 
             }
